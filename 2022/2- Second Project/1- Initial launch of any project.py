aa164f33c18788a3f4bc332f7b6d0edab927d735
Iranian Programer <93151188+sinalalebakhsh@users.noreply.github.com>
2022-05-17 21:27:10 +0430

    Update and rename home.py to 1- Initial launch of any project.py

---
+++ 2022/2- Second Project/1- Initial launch of any project.py
@@ -0,0 +1,9 @@
+# 1- Create a virtual environment
+# type this command in address Terminal => python -m venv venv
+#2- Active it with this command
+# ./venv/Scripts/Activate
+#3- Istall django
+# pip install django
+#4- build a Project django
+# django-admin startproject (name-project recommended =>) config .
+#Be sure to make the point
