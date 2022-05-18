d0c488fa56699c65035d1203318226f0d2d4814b
Iranian Programer <93151188+sinalalebakhsh@users.noreply.github.com>
2022-05-18 22:26:01 +0430

    Update urls.py

--- 2022/2- Second Project/1- homepage/home/urls.py
+++ 2022/2- Second Project/1- homepage/home/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.homepage, name='home page')
+    path('', views.homepage, name='home page'),
+    path('rooot/', views.rooot, name='rooot page'),
+    path('sinanis/', views.sinanis, name='sinanis page'),
+
 ]
