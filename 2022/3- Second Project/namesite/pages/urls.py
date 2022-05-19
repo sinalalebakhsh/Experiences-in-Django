02f3e95116649aabfb4ab1bd091eded3c1d6c20d
Iranian Programer <93151188+sinalalebakhsh@users.noreply.github.com>
2022-05-19 12:47:22 +0430

    Update urls.py

--- 2022/3- Second Project/namesite/pages/urls.py
+++ 2022/3- Second Project/namesite/pages/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('hackme.space/', views.hackme_space, name='hackme_space'),
+    path('hackme.space2022/', views.hackme_space, name='hackme.space'),
+    path('sinanis/', views.sinanis, name='hackme space'),
+
 ]
