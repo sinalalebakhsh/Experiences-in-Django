cfe6058e4e8d2465e42e8b7554c90460ebd66bd6
Iranian Programer <93151188+sinalalebakhsh@users.noreply.github.com>
2022-05-19 12:29:46 +0430

    Update views.py

--- 2022/3- Second Project/namesite/pages/views.py
+++ 2022/3- Second Project/namesite/pages/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        'page_name': 'home',
+    }
+    return render(request, 'home.html', context)
 
 
 def hackme_space(request):
     return render(request, 'pages/hackme_space.html')
+
+
+def sinanis(request):
+    return render(request, 'pages/sinanis.html')
+
 
