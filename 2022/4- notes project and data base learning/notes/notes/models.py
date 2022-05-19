ef329f956c56901ed43511a0c5528cec9a099799
Iranian Programer <93151188+sinalalebakhsh@users.noreply.github.com>
2022-05-19 19:10:08 +0430

    Create models.py

---
+++ 2022/4- notes project and data base learning/notes/notes/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Note(models.Model):
+    text = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+
+    
+    
+    
+# and write this in terminal address -->>   python manage.py makemigrations notes
