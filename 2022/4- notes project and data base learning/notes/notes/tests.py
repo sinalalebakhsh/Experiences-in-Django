2817e8986c243c8818414643ccda6f28205ba876
Iranian Programer <93151188+sinalalebakhsh@users.noreply.github.com>
2022-05-19 22:38:59 +0430

    Update tests.py

--- 2022/4- notes project and data base learning/notes/notes/tests.py
+++ 2022/4- notes project and data base learning/notes/notes/tests.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
 class ListViewTesting(TestCase):
     def test_list_view(self):
         response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_view_by_name(self):
+        response = self.client.get(reverse('list views'))
         self.assertEqual(response.status_code, 200)
 
 
