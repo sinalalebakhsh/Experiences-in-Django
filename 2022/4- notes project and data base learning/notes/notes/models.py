111fd4e09e41ed8e40e121f5e6629116b62b9047
Iranian Programer <93151188+sinalalebakhsh@users.noreply.github.com>
2022-05-19 20:10:02 +0430

    Update models.py

--- 2022/4- notes project and data base learning/notes/notes/models.py
+++ 2022/4- notes project and data base learning/notes/notes/models.py
@@ -4,11 +4,9 @@ class Note(models.Model):
     text = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
 
+    # and write this in terminal address -->>   python manage.py makemigrations notes
+    # than write terminal -->>                  python manage.py migrate
     
-    
-    
-# and write this in terminal address -->>   python manage.py makemigrations notes
-
-
-
-# than write terminal -->>                  python manage.py migrate
+    def __str__(self):
+        return (self.author , self.text)
+   
